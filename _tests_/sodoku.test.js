fc4b2c84d33779c20ca294765a3fc91b51566a96
Stephen <szook7@gmail.com>
2023-01-31 16:36:34 -0800

    added logic to check if there are duplicates in a row
    Co-authored-by: Charles <charlesbcookman@gmail.com>

--- _tests_/sodoku.test.js
+++ _tests_/sodoku.test.js
@@ -1,4 +1,4 @@
-import Rows from "./../sodoku"
+import {Rows, checkDuplicate} from "./../sodoku";
 
 describe('Rows', () => {
 
@@ -8,4 +8,10 @@ describe('Rows', () => {
         expect(row.rowSpot2).toEqual(3);
         expect(row.rowSpot3).toEqual(7);
     });
-});
+
+    test("Should test if a value has a duplicate in the row", () =>{
+        const row = [5, 3, 7];
+        checkDuplicate(row);
+        expect(checkDuplicate(row)).toEqual(false);
+        });
+    });
--- sodoku.js
+++ sodoku.js
@@ -1,7 +1,14 @@
 
 
-export default function Rows(rowSpot1, rowSpot2, rowSpot3){
+export function Rows(rowSpot1, rowSpot2, rowSpot3){
     this.rowSpot1 = rowSpot1
     this.rowSpot2 = rowSpot2
     this.rowSpot3 = rowSpot3
 }
+
+export const checkDuplicate = function(array){
+     if (new Set(array).size !== array.length){
+        return false;
+     } else {return true}
+};
+    
